1e32513d590f1a5a3d13e6641acf99033f44abe0
Shawn Bruce <kantlivelong@gmail.com>
2017-07-20 12:58:40 -0400

    Make use of the client helpers provided in OctoPrint 1.3.5 Handle result from API commands appropriately.

--- octoprint_psucontrol/__init__.py
+++ octoprint_psucontrol/__init__.py
@@ -423,50 +423,79 @@ class PSUControl(octoprint.plugin.StartupPlugin,
             return jsonify(isPSUOn=self.isPSUOn)
 
     def cli_commands(self, cli_group, pass_octoprint_ctx, *args, **kwargs):
+        # Requires OctoPrint >= 1.3.5
         import click
         import sys
+        import json
         import requests.exceptions
-        from octoprint_client import Client, build_base_url
+        from octoprint.cli.client import create_client, client_options
 
-        def _api_command(command):
-            host = cli_group.settings.get(["server", "host"])
-            host = host if host != "0.0.0.0" else "127.0.0.1"
+        def _api_command(command, apikey, host, port, httpuser, httppass, https, prefix):
+            if prefix == None:
+                prefix = '/api'
 
-            port = cli_group.settings.getInt(["server", "port"])
+            client = create_client(settings=cli_group.settings,
+                                   apikey=apikey,
+                                   host=host,
+                                   port=port,
+                                   httpuser=httpuser,
+                                   httppass=httppass,
+                                   https=https,
+                                   prefix=prefix)
 
-            baseurl = build_base_url(host=host, port=port, prefix="/api")
-
-            api_key = cli_group.settings.get(["api", "key"])
-
-            client = Client(baseurl, api_key)
             r = client.post_command("plugin/psucontrol", command)
             try:
                 r.raise_for_status()
             except requests.exceptions.HTTPError as e:
                 click.echo("HTTP Error, got {}".format(e))
                 sys.exit(1)   
-        
+
             return r
 
+        @client_options
         @click.command("on")
-        def turnPSUOn_command():
+        def turnPSUOn_command(apikey, host, port, httpuser, httppass, https, prefix):
             """Turn the PSU On"""
-            _api_command('turnPSUOn')
+            r = _api_command('turnPSUOn', apikey, host, port, httpuser, httppass, https, prefix)
 
+            if r.status_code in [200, 204]:
+                click.echo('ok')
+
+        @client_options
         @click.command("off")
-        def turnPSUOff_command():
+        def turnPSUOff_command(apikey, host, port, httpuser, httppass, https, prefix):
             """Turn the PSU Off"""
-            _api_command('off')
+            r = _api_command('turnPSUOff', apikey, host, port, httpuser, httppass, https, prefix)
 
+            if r.status_code in [200, 204]:
+                click.echo('ok')
+
+        @client_options
         @click.command("toggle")
-        def togglePSU_command():
+        def togglePSU_command(apikey, host, port, httpuser, httppass, https, prefix):
             """Toggle the PSU On/Off"""
-            _api_command('togglePSU')
+            r = _api_command('togglePSU', apikey, host, port, httpuser, httppass, https, prefix)
 
+            if r.status_code in [200, 204]:
+                click.echo('ok')
+
+        @click.option("--return-int", is_flag=True, help="Return the PSU state as a boolean integer.")
+        @client_options
         @click.command("status")
-        def getPSUState_command():
+        def getPSUState_command(return_int, apikey, host, port, httpuser, httppass, https, prefix):
             """Get the current PSU status"""
-            print _api_command('getPSUState').__dict__
+            r = _api_command('getPSUState', apikey, host, port, httpuser, httppass, https, prefix)
+
+            if r.status_code in [200, 204]:
+                data = json.loads(r._content)
+                
+                if return_int:
+                    click.echo(int(data['isPSUOn']))
+                else:
+                    if data['isPSUOn']:
+                        click.echo('on')
+                    else:
+                        click.echo('off')
 
         return [turnPSUOn_command, turnPSUOff_command, togglePSU_command, getPSUState_command]
 
